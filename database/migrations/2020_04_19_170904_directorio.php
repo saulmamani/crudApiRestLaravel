8ddd7ee2ae33139da1a3908b11de19b8b086e8ee
saulmamani <luas0_1@yahoo,es>
2020-04-19 17:13:27 -0400

    Clase numero 1

---
+++ database/migrations/2020_04_19_170904_directorio.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Directorio extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('directorio', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_completo', 100);
+            $table->string('direccion')->nullable();
+            $table->integer('telefono')->unique();
+            $table->string('foto')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('directorio');
+    }
+}
