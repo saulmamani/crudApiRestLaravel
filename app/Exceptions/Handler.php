046b4e5376fa47429b16a5aa39391727d6a361c1
saulmamani <luas0_1@yahoo,es>
2020-04-19 18:00:39 -0400

    clase 5: get show y exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,13 @@
 
 namespace App\Exceptions;
 
+use Dotenv\Exception\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -50,6 +56,26 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if($exception instanceof ModelNotFoundException){
+            return response()->json(["res" => false, "error" => "Error de modelo"], 400);
+        }
+
+        if($exception instanceof QueryException){
+            return response()->json(["res" => false, "message" => "Error de consulta BDD " , $exception->getMessage()], 500);
+        }
+
+        if($exception instanceof HttpException){
+            return response()->json(["res" => false, "message" => "Error de ruta"], 404);
+        }
+
+        if($exception instanceof AuthenticationException){
+            return response()->json(["res" => false, "message" => "Error de autenticación"], 401);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response()->json(["res" => false, "message" => "Error de autorización, no tiene permisos"], 403);
+        }
+
         return parent::render($request, $exception);
     }
 }
